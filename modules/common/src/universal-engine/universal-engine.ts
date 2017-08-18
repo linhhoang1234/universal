0ddb84d4d77f285a19d8bcea49d8eb237d4d1197
Adam Plumer <caerus.karu@gmail.com>
2017-08-18 14:23:58 +1200

    feat(common): add UniversalEngine to encapsulate rendering
    * Abstract the common rendering patterns and caching into
      a UniversalEngine
    * Update build parameters to reflect dependency on
      common for existing engines
    
    BREAKING CHANGE:
    
    * The ASP.NET Core, Express, and Hapi engines now have
      the Common package as a peer dependency

---
+++ modules/common/src/universal-engine/universal-engine.ts
@@ -0,0 +1,97 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import {ResourceLoader} from '@angular/compiler';
+import {Compiler, Type, NgModuleFactory, CompilerFactory, StaticProvider} from '@angular/core';
+import {INITIAL_CONFIG, renderModuleFactory, platformDynamicServer} from '@angular/platform-server';
+import * as fs from 'fs';
+
+import {FileLoader} from './file-loader';
+import {RenderOptions} from './interfaces';
+
+/**
+ * A common rendering engine utility. This abstracts the logic
+ * for handling the platformServer compiler, the module cache, and
+ * the document loader
+ * @internal
+ */
+export class ɵUniversalEngine { // tslint:disable-line
+
+  /**
+   * Return an instance of the platformServer compiler
+   * @internal
+   */
+  static getCompiler(): Compiler {
+    const compilerFactory: CompilerFactory = platformDynamicServer().injector.get(CompilerFactory);
+    return compilerFactory.createCompiler([
+      {providers: [{provide: ResourceLoader, useClass: FileLoader, deps: []}]}
+    ]);
+  }
+
+  private factoryCacheMap = new Map<Type<{}>, NgModuleFactory<{}>>();
+  private templateCache: {[key: string]: string} = {};
+
+  constructor(private moduleOrFactory: Type<{}> | NgModuleFactory<{}>,
+              private providers: StaticProvider[] = []) {}
+
+  /**
+   * Render an HTML document for a specific URL with specified
+   * render options
+   * @internal
+   */
+  render(filePath: string, url: string, opts: RenderOptions): Promise<string> {
+    const extraProviders = [
+      ...(opts.providers || []),
+      ...(this.providers || []),
+      [
+        {
+          provide: INITIAL_CONFIG,
+          useValue: {
+            document: this.getDocument(filePath),
+            url
+          }
+        }
+      ]
+    ];
+
+    return this.getFactory()
+      .then(factory => renderModuleFactory(factory, {extraProviders}));
+  }
+
+  /**
+   * Return the factory for a given engine instance
+   * @internal
+   */
+  getFactory(): Promise<NgModuleFactory<{}>> {
+    // If module has been compiled AoT
+    const moduleOrFactory = this.moduleOrFactory;
+    if (moduleOrFactory instanceof NgModuleFactory) {
+      return Promise.resolve(moduleOrFactory);
+    } else {
+      // we're in JIT mode
+      let moduleFactory = this.factoryCacheMap.get(moduleOrFactory);
+
+      // If module factory is cached
+      if (moduleFactory) {
+        return Promise.resolve(moduleFactory);
+      }
+
+      // Compile the module and cache it
+      return ɵUniversalEngine.getCompiler().compileModuleAsync(moduleOrFactory)
+        .then((factory) => {
+          this.factoryCacheMap.set(moduleOrFactory, factory);
+          return factory;
+        });
+    }
+  }
+
+  /** Retrieve the document from the cache or the filesystem */
+  private getDocument(filePath: string): string {
+    return this.templateCache[filePath] = this.templateCache[filePath] ||
+      fs.readFileSync(filePath).toString();
+  }
+}
